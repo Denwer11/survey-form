147994f28466608a232fa830b8181ec1e2d6270e
Denwer1 <denwer0204@gmail.com>
2024-03-21 21:28:53 +0300

    edits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer class="bg-body-tertiary text-center text-lg-start">
+      <div
+        class="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+      >
+        <Link
+          class="text-body text-decoration-none"
+          to="https://github.com/Denwer11"
+        >
+          © 2024 Copyright: Даша
+        </Link>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,31 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <footer class="bg-body-tertiary text-center text-lg-start">
-      <div
-        class="text-center p-3"
-        style={{backgroundColor: 'rgba(0, 0, 0, 0.05)'}}
-      >
-        © 2024 Copyright:
-        <a class="text-body" href="https://github.com/Denwer11">
-          Даша
+    <div class="container">
+      <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a
+          href="/"
+          class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+        >
+          <span class="fs-2 text-success">Survey form</span>
         </a>
-      </div>
-    </footer>
+
+        <ul class="nav nav-pills">
+          <li class="nav-item">
+            <Link to="/" class="nav-link text-success" aria-current="page">
+              Главная
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link to="/about" class="nav-link text-success">
+              О нас
+            </Link>
+          </li>
+        </ul>
+      </header>
+    </div>
   );
 };
 
